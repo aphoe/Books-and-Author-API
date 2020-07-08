e7d99abfe1696e32af0114ce38d672c932e978f6
aphoe <aphoe@outlook.com>
2020-07-08 09:09:17 +0100

    - Doc blocks for models

--- app/Author.php
+++ app/Author.php
@@ -5,6 +5,24 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Author
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Book[] $books
+ * @property-read int|null $books_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Author extends Model
 {
     /**
--- app/Book.php
+++ app/Book.php
@@ -5,6 +5,34 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Book
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $isbn
+ * @property string $country
+ * @property int $number_of_pages
+ * @property string $publisher
+ * @property \Illuminate\Support\Carbon $release_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Author[] $authors
+ * @property-read int|null $authors_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereIsbn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereNumberOfPages($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book wherePublisher($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereReleaseDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Book whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Book extends Model
 {
     protected $dates = [
